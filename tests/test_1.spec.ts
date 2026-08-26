304057c5688b9b48f1e84eae0a06a65cb583498f
makajjou <makajjou@gmail.com>
2026-08-26 20:59:55 +0200

    first commit

---
+++ tests/test_1.spec.ts
@@ -0,0 +1,12 @@
+import { test, expect } from '@playwright/test';
+
+
+test('test_1', async({page}) => {
+
+await page.goto("https://playwright.dev");
+await page.waitForTimeout(5000);
+
+await 
+});
+
+
